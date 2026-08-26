3a0a3d0bfdfc4e95290f7c8be94a136dd3be9e1a
Asaf <idenauth@protonmail.com>
2026-08-25 20:42:45 -0600

    Mover server.js a la raiz para corregir despliegue en Railway

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la carpeta public
-app.use(express.static(path.join(__dirname, '../public')));
+// Servir archivos estáticos desde la carpeta public (ahora en la misma ruta raíz)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta comodín para SPA o carga de la landing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
